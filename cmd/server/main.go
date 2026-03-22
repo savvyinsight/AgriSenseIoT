07cf23a06ca4a2ec77a7cb76630d7f86a2f7b07e
savvyinsight <ecedwdpf@outlook.com>
2026-03-22 15:57:00 +0800

    update:update cors url to deploy

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -136,7 +136,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"}, // frontend URL
+		AllowOrigins:     []string{"http://localhost:5173", "https://agrisense-frontend-bice.vercel.app"}, // frontend URL
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
